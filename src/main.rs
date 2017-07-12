dcb0cc24f49a73808082a5473c1bbe307c2d8b16
gitbugr <kyronalextaylor@gmail.com>
2017-07-12 01:41:29 +0100

    Checks ROM Header, More Notes, Starting to get a plan

--- src/main.rs
+++ src/main.rs
@@ -30,39 +30,79 @@ pub struct NESEmulator {
     p: u8, // Negative, oVerflow, ss, Decimal, Interupt, Zero, Carry
 
     // Memory
-    ram: [u8; 2047], // 2kb of console memory
+    cpu_memory: [u8; 0x10000],
 
     // Clock Speed
-    clock: f32
+    clock: f32,
 
+    // File Path
+    filepath: String
 }
 
 // implimentation
 impl NESEmulator {
     // initializes registers
-    fn new() -> NESEmulator {
+    pub fn new(f: &String) -> NESEmulator {
         return NESEmulator {
-            a: 0x0000,
-            x: 0x0000,
-            y: 0x0000,
-            sp: 0x0000,
-            pc: 0xC000,
+            a: 0x00,
+            x: 0x00,
+            y: 0x00,
+            sp: 0x00,
+            pc: 0xC000, // start reading instructions from byte 0xC000
             p: 0b00000000,
-            ram: [0u8; 2047],
-            clock: 1.79
+            cpu_memory: [0u8; 0x10000],
+            clock: 1.79, // US-region
+            filepath: f.to_owned()
         }
+    }
+
+    pub fn load_rom(&self){
+        let mut file = File::open(&self.filepath).expect("ERROR: File not found"); // load file
+        let mut buffer = Vec::new(); // definte buffur vector
+        file.read_to_end(&mut buffer); // store bytes in buffer
+
+        if(b"NES" == &buffer[0..3]) { println!("Passed header check!"); }
+        else { println!("Failed header check!"); }
+    }
+
+    // (memory mapper) memory i/o
+    fn read_mem() {
+
+    }
+
+    fn write_mem(){
+
+    }
+
+    // reads instructions, calls functions, sleeps
+    fn tick(&self) {
+
+    }
+
+    fn run() {
+
     }
 }
 
 fn main(){
+    // Gets ROM filename from user argument and loads it into a buffer
     let args: Vec<String> = env::args().collect();
-    if(args.len() > 1){
-        println!("Opening ROM: {}",&args[1]);
+    if args.len() > 1 {
+        println!("--------------------------------------------");
+        println!("\\ Nintendo Entertainment System Emulator  /");
+        println!("/    Written by Kyron Taylor (gitbugr)    \\");
+        println!("--------------------------------------------");
+        println!("Starting NES Emulator with Default Values...");
+        let mut emu = NESEmulator::new(&args[1]);
 
-        let file = File::open(&args[1]).expect("ERROR: File not found");
+        println!("Opening ROM: '{}'",&args[1]); // debug
+        emu.load_rom();
+
+
+
     }
     else{
-        println!("Please specify a ROM")
+        println!("Please specify a ROM"); // no args
     }
     return();
 }
